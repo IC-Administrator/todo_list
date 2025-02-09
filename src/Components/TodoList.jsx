0215c65d436f9d9067f1d6fc42a0dc109a0dbc09
IC-Administrator <invisible.college.education@gmail.com>
2025-02-09 09:37:27 +0000

    updated TodoList.jsx

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
+  const [todos, setTodos] = useState([]); // initialises "todos" state representing an array of todo items with an empty array
+  const [headingInput, setHeadingInput] = useState(' '); // initialises "headingInput" state representing the value entered by user into input field for adding a new heading for a todo item, initialised as an empty string
+  const [listInputs, setListInputs] = useState({}); // initialises "listInputs" state as an empty object {}.  This state will hold the values of input fields for each todo item individually.
   
-
   return (
     <>
       <div className="todo-container">
